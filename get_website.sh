f7c6227dd5dbd30034fd174481c1ba2fe2af5c44
znwang25 <znwang25@gmail.com>
2021-03-13 19:28:55 -0800

    Add script to auto download the site

---
+++ get_website.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+wget \
+     --recursive \
+     --span-hosts \
+     --adjust-extension \
+     --compression=auto \
+     -e robots=off \
+     --convert-links -Duploads-ssl.webflow.com,webflow.io \
+     --directory-prefix=download_from_webflow/ https://siyu-portfolio.webflow.io/
+
+mv download_from_webflow/siyu-portfolio.webflow.io/*  download_from_webflow/
+rm -r download_from_webflow/siyu-portfolio.webflow.io
+
+mv download_from_webflow/uploads-ssl.webflow.com/603c54506a6e6f2e48a72066/ download_from_webflow/assets/
+
+rm -r download_from_webflow/uploads-ssl.webflow.com
+
+path_old='../uploads-ssl.webflow.com/603c54506a6e6f2e48a72066/'
+path_new='assets/'
+
+grep -rli $path_old download_from_webflow/ | xargs -I@ sed -i '' "s%$path_old%$path_new%g"  @
+ 
+weflow_badge_css='.w-webflow-badge,.w-webflow-badge.*vertical-align:middle !important}'
+new_weflow_badge_css='.w-webflow-badge,.w-webflow-badge *{display:none;}.w-webflow-badge{display:none;}.w-webflow-badge>img{display:none;}'
+
+grep -rli $weflow_badge_css download_from_webflow/assets/css| xargs -I@ sed -i '' "s%$weflow_badge_css%$new_weflow_badge_css%g"  @
+
+find download_from_webflow/ -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | xargs -I@ js-beautify @ -o @
